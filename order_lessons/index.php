784c340fdda7a081d195e898889cd65b128aa52b
aaa <aaa@aaa.com>
2019-03-16 17:36:41 +0200

    order_lesson 02

---
+++ order_lessons/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require ('order_lesson.php');
+
+?>
--- order_lessons/order_lesson.php
+++ order_lessons/order_lesson.php
@@ -3,6 +3,7 @@
 <title>Worklog dim</title>
 <META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
 <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 </head>
 <?php
 
